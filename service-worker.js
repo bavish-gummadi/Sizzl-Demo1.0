6b6f8b50c972239374f99baef98130279fe4d015
gummadib <gummadib@umich.edu>
2018-05-13 21:45:47 -0400

    bottom toolbar and saladsup

--- service-worker.js
+++ service-worker.js
@@ -7,19 +7,10 @@ var filesToCache = [
   '/index.html',
   '/scripts/app.js',
   '/styles/inline.css',
-  '/images/clear.png',
-  '/images/cloudy-scattered-showers.png',
-  '/images/cloudy.png',
-  '/images/fog.png',
-  '/images/ic_add_white_24px.svg',
-  '/images/ic_refresh_white_24px.svg',
-  '/images/partly-cloudy.png',
-  '/images/rain.png',
-  '/images/scattered-showers.png',
-  '/images/sleet.png',
-  '/images/snow.png',
-  '/images/thunderstorm.png',
-  '/images/wind.png'
+  '/images/pieology.jpeg',
+  '/images/saladsup.jpg',
+  'images/icons/SizzlWSq256.png',
+  '/images/searchw.png',
 ];
 //Keeps track of the most updated data in the chache
 var dataCacheName = 'weatherData-v1';
